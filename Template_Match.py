b57a904e20851d37bdc94ed59eb0616a178a9f2f
AyubSherif <70292001+AyubSherif@users.noreply.github.com>
2022-04-04 07:34:09 -0400

    Update Template_Match.py

--- Template_Match.py
+++ Template_Match.py
@@ -1,17 +1,8 @@
 def tamplate_match(in1, in2, mode='full', method='auto'):
     r"""
     Cross-correlate two N-dimensional arrays.
-    
-    in1 : array_like
-        First input.
-    in2 : array_like
-        Second input. Should have the same number of dimensions as `in1`.
-    
-    Returns
-    
-    correlate : array
-        An N-dimensional array containing a subset of the discrete linear
-        cross-correlation of `in1` with `in2`.
+    Second input. Should have the same number of dimensions as in1. 
+    Returns An N-dimensional array containing a subset of the discrete linear cross-correlation of in1 with in2.
     
     """
     in1 = np.asarray(in1)
